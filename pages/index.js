606f488358f23cdd03c9e2eb19f3e713e64f2f29
ihectorlujan <ihectorlujan96@gmail.com>
2022-03-10 11:38:09 -0600

    solve crossOrigin

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
     <MainContainer>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Spline+Sans&display=swap"
           rel="stylesheet"
