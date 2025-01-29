14c73af596517014d69a68d7f25321c9c77f7c14
ihectorlujan <ihectorlujan96@gmail.com>
2025-01-29 10:05:37 -0600

    Fix: cambios en las animaciones

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,8 +12,9 @@ const Navbar: React.FC<Props> = ({ children }) => {
 
   return (
     <div className="flex justify-center m-6">
-      <div className="rounded-3xl bg-gray-100 min-h-[50px] w-2/3 max-w-[600px] p-3">
+      <div className="rounded-3xl bg-gray-100/50 min-h-[50px] w-5/6 max-w-[600px] p-3 backdrop-blur-sm">
         <span className="hidden md:flex md:flex-row md:justify-evenly">
+          <a className="font-sans font-semibold text-md mx-1 p-1">Hector</a>
           {children}
         </span>
         <span className="flex flex-col justify-between md:hidden">
@@ -22,11 +23,19 @@ const Navbar: React.FC<Props> = ({ children }) => {
             <XButton isOpen={isOpen} setIsOpen={setIsOpen} />
           </div>
           <div
-            className={`transition-all duration-[0.3s] ease-in-out overflow-hidden ${
+            className={`transition-all duration-[0.4s] linear overflow-hidden ${
               isOpen ? "max-h-[500px]" : "max-h-0"
             }`}
           >
-            <div>{children}</div>
+            <div
+              className={`transition-all duration-[0.4s] linear ${
+                isOpen
+                  ? "opacity-100 translate-y-0"
+                  : "opacity-0 -translate-y-2"
+              }`}
+            >
+              {children}
+            </div>
           </div>
         </span>
       </div>
