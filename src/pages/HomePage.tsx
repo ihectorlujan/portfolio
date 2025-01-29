3cbc06b138dd8e679dae017b6142b44b91e3d8ef
ihectorlujan <ihectorlujan96@gmail.com>
2025-01-29 12:12:33 -0600

    Feat: creacion de shape divider

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import styles from "@/styles/Shape.module.css";
+import ShapeDivider from "@/components/ShapeDivider";
 
 const Home = () => {
   return (
     <>
-      <section className="h-screen flex flex-col justify-end">
+      <section className="h-screen">
+        <ShapeDivider />
         <div className="flex flex-col items-center m-5">
           <ChevronDownIcon className="h-6 w-6" />
           <p>Scroll for more!</p>
